4bae8b6aa816af66529aa1facf836673b14b4206
Evan Slaton <evanslaton@gmail.com>
2018-09-25 17:50:57 -0700

    changes

--- server.js
+++ server.js
@@ -50,8 +50,8 @@ function Book(book) {
   this.title = book.title || 'Title not available';
   this.author = book.authors.reduce((accumulator, currentValue) => accumulator + `, ${currentValue}`) || 'Author not available';
   this.isbn = book.industryIdentifiers[0].type || 'ISBN not available';
-  // this.image_url = book.imageLinks.smallThumbnail || placeholderImage;
-  console.log(book.imageLinks);
+  this.image_url = placeholderImage;
+  console.log(placeholderImage);
   this.description = book.description || 'No description';
   this.category = book.categories[0] || 'No category';
 }
