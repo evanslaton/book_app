64b36d1a4e9bf059ced02c811165c0d6cd3a4957
RazorWire13 <baneblade13@gmail.com>
2018-09-27 09:11:43 -0700

    housecleaning on files

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,6 @@ function revealForm(event) {
 
 // Hides the form when the user decides not to add a book to the database
 function hideForm(event) {
-  event.preventDefault();
   const clickedOn = event.target;
   if (event.target.textContent === 'Hide Form') {
     $(clickedOn).parent().addClass('hidden');
