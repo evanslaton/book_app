72acf13fa8b393f130369df8aa5ac194800a6ff5
Evan Slaton <evanslaton@gmail.com>
2018-09-26 19:16:57 -0700

    Form is revealed or hidden based on user selection

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
-function unHide(event) {
+// Reveals the form when the user wants to add a book to the database
+function revealForm(event) {
   event.preventDefault();
   const clickedOn = event.target;
   if (event.target.textContent === 'Select this Book') {
@@ -8,4 +9,17 @@ function unHide(event) {
   }
 }
 
-$('.show-add-form').on('click', unHide);
+// Hides the form when the user decides not to add a book to the database
+function hideForm(event) {
+  event.preventDefault();
+  const clickedOn = event.target;
+  if (event.target.textContent === 'Hide Form') {
+    $(clickedOn).parent().addClass('hidden');
+  }
+}
+
+$('.show-add-form').on('click', (event) => {
+  revealForm(event);
+  hideForm(event);
+});
+
